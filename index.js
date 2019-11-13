05ed4d48be553c06a176335e3d293a13b6c46ff2
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2019-11-13 20:49:03 +0000

    make it better

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const express = require('express');
 const cors = require('cors');
+const puppeteer = require("puppeteer");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.post('/fetch-menu-items', (req, res) => {
-   const url = req.body.urltoscan;
+   const url = "https://www.just-eat.co.uk/restaurants-milano-pizza-new-bow/menu";
+   (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const divClass = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("div.information")).map(arr => arr.innerText.trim())
+    );
+    res.send(divClass);
+    await browser.close();
+  })();
 
-
-  fetch(`${url}`)
-    .then(page => page.text()) 
-    .then(body => {
-      const menuItems = [];
-      const $ = cheerio.load(body);
-      $('.menu-item').each(function fn() {
-        const _id = new Date().getTime()+Math.floor(Math.random()*100000);
-        const dish = $(this).find('h4').text();
-        const ingredients = $(this).find('p').text();
-        menuItems.push({_id, dish, ingredients });
-      });
-      res.send(menuItems);
-    });
-});
-
-app.listen(process.env.PORT || 3001);
-
-module.exports = app;
+  });
+  
+  app.listen(process.env.PORT || 3002);
+  
+  module.exports = app;
+ 
